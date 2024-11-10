cfe1798ea96bf0ff4c7965ca3de8cccfc97d715f
Claudio <claudio.marino@outlook.com>
2024-11-10 15:13:10 +0100

    ok

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,11 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
 	return (
-		<main>
+		<main
+			className="main"
+			style={{
+				backgroundImage: `url("https://imgur.com/3jjiEre.jpg")`,
+			}}>
 			<div className="main-wrap">
 				<p>Il bello di sentirsi protetti.</p>
 				<h1>SCOPRI IL PRODOTTO CHE FA PER TE!</h1>

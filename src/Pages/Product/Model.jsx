5c143a227c19fa611687a4246d8906e4c6b364d5
Claudio <claudio.marino@outlook.com>
2024-11-28 16:48:44 +0100

    ok

--- src/Pages/Product/Model.jsx
+++ src/Pages/Product/Model.jsx
@@ -16,15 +16,19 @@ export default function Model() {
 		<div
 			key={renderModel[0].name}
 			className="programas-wrap model">
-			<img
-				src={renderModel[0].image}
-				alt={`image of ${renderModel[0].name}`}
-			/>
 			<h2>{renderModel[0].name}</h2>
 			<p>{renderModel[0].description}</p>
-			<div className="varianti">
-				{renderModel[0].varianti &&
-					renderModel[0].varianti.map((variante, index) => <h3 key={index}>{variante}</h3>)}
+			<div className="upper-container">
+				<div className="upper-left">
+					<img
+						src={renderModel[0].image}
+						alt={`image of ${renderModel[0].name}`}
+					/>
+				</div>
+				<div className="varianti">
+					{renderModel[0].varianti &&
+						renderModel[0].varianti.map((variante, index) => <h3 key={index}>{variante}</h3>)}
+				</div>
 			</div>
 			<div className="caratteristiche">
 				{renderModel[0].scorrimento && (

0c4d51bbf202c1edf7cdb05a47cc967a2ac5479b
Claudio <claudio.marino@outlook.com>
2024-12-02 16:41:03 +0100

    ok

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,41 +2,44 @@ export default function About() {
 	return (
 		<div className="about">
 			<h1>Chi siamo</h1>
-			<p>
-				La <strong>My System</strong>, azienda Campana, da 30 anni creiamo idee e prodotti;
-				specializzata nella produzione industriale di
-				<strong> sistemi filtranti ed oscuranti contro gli insetti</strong> e nella produzione e
-				commercializzazione di{' '}
-				<strong>
-					tapparelle, frangisole, porte a soffietto e box doccia, meccanismi brevettati
-				</strong>
-				, sicurezza certificata dalla marcatura CE.
-			</p>
-			<img
-				src="https://imgur.com/6gQbiKx.jpg"
-				alt="immagine della facciata di My System"
-			/>
-			<p>
-				Idee differenti, uniche, perfino rivoluzionarie, apprezzate per la loro capacità di portare
-				un cambiamento e creare valore aggiunto. <strong>Innovazione e qualità</strong> sono stati e
-				continuano ad essere il nostro punto di forza con lo sguardo sempre orientato al futuro.
-			</p>
-			<p>
-				L’elevata automazione del flusso di produzione, efficienza e precisione consente all’azienda
-				di sviluppare una <strong>notevole capacità produttiva</strong> che si ritrovano
-				nell’organizzazione di consulenza/vendita per un tempo medio di consegna su tutto il
-				territorio nazionale di circa <strong>3 giorni lavorativi</strong> a partire dall’ordine e
-				nel servizio di postvendita che accompagna i prodotti My System,
-				<strong> assistendo il cliente prima e dopo l’installazione.</strong>
-			</p>
-			<p>
-				Ogni modello a catalogo prevede un <strong>articolato assortimento di colori</strong> tra
-				cui scegliere, sia per i profili sia per la tipologia della rete o del telo oscurante. Con
-				l’obiettivo di essere <strong>sempre in linea con le tendenze del mercato</strong>, grazie
-				al reparto di Ricerca e Sviluppo, le zanzariere e gli avvolgibili sono costantemente
-				aggiornati attraverso l’adozione di sistemi innovativi di installazione o di utilizzo che
-				possano favorire la competitività del prodotto.
-			</p>
+			<div className="container">
+				<p>
+					La <strong>My System</strong>, azienda Campana, da 30 anni creiamo idee e prodotti;
+					specializzata nella produzione industriale di
+					<strong> sistemi filtranti ed oscuranti contro gli insetti</strong> e nella produzione e
+					commercializzazione di{' '}
+					<strong>
+						tapparelle, frangisole, porte a soffietto e box doccia, meccanismi brevettati
+					</strong>
+					, sicurezza certificata dalla marcatura CE.
+				</p>
+				<img
+					src="https://imgur.com/6gQbiKx.jpg"
+					alt="immagine della facciata di My System"
+				/>
+				<p>
+					Idee differenti, uniche, perfino rivoluzionarie, apprezzate per la loro capacità di
+					portare un cambiamento e creare valore aggiunto. <strong>Innovazione e qualità</strong>{' '}
+					sono stati e continuano ad essere il nostro punto di forza con lo sguardo sempre orientato
+					al futuro.
+				</p>
+				<p>
+					L’elevata automazione del flusso di produzione, efficienza e precisione consente
+					all’azienda di sviluppare una <strong>notevole capacità produttiva</strong> che si
+					ritrovano nell’organizzazione di consulenza/vendita per un tempo medio di consegna su
+					tutto il territorio nazionale di circa <strong>3 giorni lavorativi</strong> a partire
+					dall’ordine e nel servizio di postvendita che accompagna i prodotti My System,
+					<strong> assistendo il cliente prima e dopo l’installazione.</strong>
+				</p>
+				<p>
+					Ogni modello a catalogo prevede un <strong>articolato assortimento di colori</strong> tra
+					cui scegliere, sia per i profili sia per la tipologia della rete o del telo oscurante. Con
+					l’obiettivo di essere <strong>sempre in linea con le tendenze del mercato</strong>, grazie
+					al reparto di Ricerca e Sviluppo, le zanzariere e gli avvolgibili sono costantemente
+					aggiornati attraverso l’adozione di sistemi innovativi di installazione o di utilizzo che
+					possano favorire la competitività del prodotto.
+				</p>
+			</div>
 		</div>
 	)
 }
